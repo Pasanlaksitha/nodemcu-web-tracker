b3795872406ba6e369fc5ae68f268375b9e784b4
Pasan Laksitha <pasanlaksithakulathunge@gmail.com>
2021-09-14 18:37:11 +0530

    Add files via upload

--- Database_IP_entry.ino
+++ Database_IP_entry.ino
@@ -1,28 +1,33 @@
+
+#include <Arduino_JSON.h>
 #include <NTPClient.h>
 #include <ESP8266WiFi.h>
+#include <WiFiClient.h> 
+#include <ESP8266HTTPClient.h>
 #include <WiFiUdp.h>
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-
 Adafruit_SSD1306 display(-1);
 
 const char *ssid     = "ISL-PC 5440";
 const char *password = "ISL123456";
 
 const long utcOffsetInSeconds = 19080;
-
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
 // Define NTP Client to get time
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
 
+const char *host1 = "http://arduinojson.org/example.json";
+const char *host2 = "http://arduinojson.org/example.json";
+const char *host3 = "http://arduinojson.org/example.json";
+
 void setup(){
   Serial.begin(115200);
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-
   WiFi.begin(ssid, password);
 
   while ( WiFi.status() != WL_CONNECTED ) {
@@ -35,7 +40,12 @@ void setup(){
 
 void loop() {
   timeClient.update();
+  void realtime();
+  void webjson();
+}
 
+void realtime(){
+ 
   Serial.print(daysOfTheWeek[timeClient.getDay()]);
   Serial.print(", ");
   Serial.print(timeClient.getHours());
@@ -59,7 +69,55 @@ void loop() {
   display.println(daysOfTheWeek[timeClient.getDay()]);
   display.display();
   
-
-
   delay(1000);
+}
+
+
+
+void webjson() {
+  HTTPClient http;    //Declare object of class HTTPClient
+
+  Serial.print("Request Link:");
+  Serial.println(host1);
+  
+  http.begin(host);     //Specify request destination
+  
+  int httpCode = http.GET();            //Send the request
+  String payload = http.getString();    //Get the response payload from server
+
+  Serial.print("Response Code:"); //200 is OK
+  Serial.println(httpCode);   //Print HTTP return code
+
+  Serial.print("Returned data from Server:");
+  Serial.println(payload);    //Print request response payload
+  
+  if(httpCode == 200)
+  {
+    // Allocate JsonBuffer
+    // Use arduinojson.org/assistant to compute the capacity.
+    const size_t capacity = JSON_OBJECT_SIZE(3) + JSON_ARRAY_SIZE(2) + 60;
+    DynamicJsonBuffer jsonBuffer(capacity);
+  
+   // Parse JSON object
+    JsonObject& root = jsonBuffer.parseObject(payload);
+    if (!root.success()) {
+      Serial.println(F("Parsing failed!"));
+      return;
+    }
+  
+    // Decode JSON/Extract values
+    Serial.println(F("Response:"));
+    Serial.println(root["sensor"].as<char*>());
+    Serial.println(root["time"].as<char*>());
+    Serial.println(root["data"][0].as<char*>());
+    Serial.println(root["data"][1].as<char*>());
+  }
+  else
+  {
+    Serial.println("Error in response");
+  }
+
+  http.end();  //Close connection
+  
+  delay(5000);  //GET Data at every 5 seconds
 }
